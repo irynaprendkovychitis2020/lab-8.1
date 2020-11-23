a57e2ef1102f925a35cf8f645ecc963f4f5d05b2
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2020-11-23 21:20:38 +0200

    lab 8.1

---
+++ lab8.1(1)/lab8.1(1)/lab8.1(1).cpp
@@ -0,0 +1,68 @@
+﻿// lab8.1(1).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* str)
+{
+	if (strlen(str) < 3)
+		return 0;
+	int k = 0;
+	for (int i = 1; str[i + 1] != 0; i++)
+		if (str[i - 1] == 'n' && str[i + 1] == 'o')
+			k++;
+	return k;
+}
+char* Change(char* str)
+{
+	if (strlen(str) < 3)
+		return str;
+	char* tmp = new char[strlen(str) * 4 / 3 + 1];
+	char* t = tmp;
+	tmp[0] = '\0';
+	int i = 0;
+	while (str[i + 2] != 0)
+	{
+		if (str[i] == 'n' && str[i + 2] == 'n')
+		{
+				strcat(t, "**");
+			t += 4;
+			i += 3;
+		}
+		else
+		{
+			*t++ = str[i++];
+			*t = '\0';
+		}
+	}
+	*t++ = str[i++];
+	*t++ = str[i++];
+	*t = '\0';
+	strcpy(str, tmp);
+	return tmp;
+}
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "String contained " << Count(str) << " groups of 'n o'" << endl;
+	char* dest = new char[151];
+	dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
